3854caebc8d92ac631fa1c1bd720865c7a700f19
Bram Reuling <bram.reuling@outlook.com>
2022-07-16 20:00:22 +0200

    Added SimpleTest

--- Engine/Core/tests/SimpleTest.cpp
+++ Engine/Core/tests/SimpleTest.cpp
@@ -1,12 +1,12 @@
 #include "gtest/gtest.h"
 #include "Core/Test.hpp"
 
-TEST(One, EqualsOne)
+TEST(SimpleTest, EqualsOne)
 {
     EXPECT_EQ(1, 1);
 }
 
-TEST(StringTest, EqualsString)
+TEST(SimpleTest, EqualsString)
 {
     EXPECT_EQ(Core::Test::ReturnFive(), 5);
 }
